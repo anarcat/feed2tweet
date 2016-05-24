dded760d3c7ea1ee8dae37aef76edba1a00a7c79
Carl Chenet <chaica@ohmytux.com>
2016-05-24 16:26:24 +0200

    remove feed2tweet sample

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
     name='feed2tweet',
     version='0.4',
     license='GNU GPL v3',
-    data_files = ['feed2tweet-sample.ini'],
     description='Parse rss feed and tweet new posts to Twitter',
     long_description='Parse rss feed and tweet new posts to Twitter',
     author = 'Carl Chenet',
